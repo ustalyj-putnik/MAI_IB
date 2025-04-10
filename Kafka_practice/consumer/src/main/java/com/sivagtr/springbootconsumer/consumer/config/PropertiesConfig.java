353f5df643f9c783acc9fce3fda51356e5436a9b
Nikita Novikov <144739976+ustalyj-putnik@users.noreply.github.com>
2025-04-10 17:03:37 +0300

    Add files via upload

---
+++ Kafka_practice/consumer/src/main/java/com/sivagtr/springbootconsumer/consumer/config/PropertiesConfig.java
@@ -0,0 +1,26 @@
+package com.sivagtr.springbootconsumer.consumer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+
+@Configuration
+@Getter
+public class PropertiesConfig {
+	@Value("${msg.topic}")
+	private String msgTopic;
+
+	@Value("${json.topic}")
+	private String jsonTopic;
+
+	@Value("${kafka.server}")
+	private String kafkaServer;
+
+	@Value("${key}")
+	private String Key;
+
+	@Value("${msgValue}")
+	private String msgValue;
+
+}
